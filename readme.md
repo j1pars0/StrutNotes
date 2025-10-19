2e18a2df800ecb662295ff834237d020435a005d
Jim Parsons <jim@jamesrparsons.co.uk>
2025-10-19 20:07:12 +0100

    Basic hello

---
+++ readme.md
@@ -0,0 +1,7 @@
+# StrutNotes
+
+***
+
+## Description
+A test of Struts 2 V 7.0 to create a simple Note application
+
---
+++ src/main/java/uk/co/jamesrparsons/strutnotes/action/NotelistAction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.co.jamesrparsons.strutnotes.action;
+
+import org.apache.struts2.ActionSupport;
+
+/**
+ *
+ * @author james
+ */
+public class NotelistAction extends ActionSupport {
+    
+}
